1e103cb8268542023b4a9827155d3431d647968b
jsonchen0702 <jsonchen0702@gmail.com>
2016-06-16 11:29:07 -0700

    Add functionality for clicking star ratings to add them to a feedback object on an item

--- app/components/feedback-tile.js
+++ app/components/feedback-tile.js
@@ -2,8 +2,56 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  // displayStarRating: Ember.computed('feedback.rating', function() {
-  //
-  // }),
+  displayStarRating: Ember.computed('feedback.rating', function() {
+    if (this.get('feedback.rating') === 1) {
+      return Ember.String.htmlSafe(
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>'
+      );
+    } else if (this.get('feedback.rating') === 2) {
+      return Ember.String.htmlSafe(
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>'
+      );
+    } else if (this.get('feedback.rating') === 3) {
+      return Ember.String.htmlSafe(
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>'
+      );
+    } else if (this.get('feedback.rating') === 4) {
+      return Ember.String.htmlSafe(
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>'
+      );
+    } else if (this.get('feedback.rating') === 5) {
+      return Ember.String.htmlSafe(
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>' +
+        '<b class="glyphicon glyphicon-star"></b>'
+      );
+    } else {
+      return Ember.String.htmlSafe(
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>' +
+        '<b class="glyphicon glyphicon-star-empty"></b>'
+      );
+    }
+  }),
 
 });
--- app/components/new-feedback.js
+++ app/components/new-feedback.js
@@ -1,12 +1,16 @@
 import Ember from 'ember';
 import moment from 'moment';
 
+var clearStars = function() {
+  $('.star-rating-selection').removeClass('selected-rating');
+}
+
 export default Ember.Component.extend({
   actions: {
     saveFeedback() {
       var params = {
         username: this.get('username'),
-        rating: parseInt(this.get('rating')),
+        rating: 0,
         comment: this.get('comment'),
         time: moment().format('LL'),
         item: this.get('item'),
@@ -15,6 +19,32 @@ export default Ember.Component.extend({
       this.set('username', "");
       this.set('rating', "");
       this.set('comment', "");
+      clearStars();
+    },
+    rateOneStar() {
+      clearStars();
+      this.set('rating', 1);
+      $('#rateOneStar').addClass('selected-rating');
+    },
+    rateTwoStar() {
+      clearStars();
+      this.set('rating', 2);
+      $('#rateTwoStar').addClass('selected-rating');
+    },
+    rateThreeStar() {
+      clearStars();
+      this.set('rating', 3);
+      $('#rateThreeStar').addClass('selected-rating');
+    },
+    rateFourStar() {
+      clearStars();
+      this.set('rating', 4);
+      $('#rateFourStar').addClass('selected-rating');
+    },
+    rateFiveStar() {
+      clearStars();
+      this.set('rating', 5);
+      $('#rateFiveStar').addClass('selected-rating');
     },
   }
 });
